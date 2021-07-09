35ee5acc41a3733d3475e62c6c2fa1ab29000f97
Gregorius Soedharmo <arkatufus@yahoo.com>
2021-07-10 03:25:28 +0700

    Add Akka.Discovery.AwsApi package

---
+++ src/discovery/Akka.Discovery.AwsApi/Ecs/EcsServiceDiscovery.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.Util;
+using Amazon;
+using Amazon.ECS;
+using Amazon.ECS.Model;
+using EcsTask = Amazon.ECS.Model.Task;
+
+namespace Akka.Discovery.AwsApi.Ecs
+{
+    public sealed class EcsServiceDiscovery : ServiceDiscovery
+    {
+        private static Either<string, IPAddress> ContainerAddress
+        {
+            get
+            {
+                var hostName = Dns.GetHostName();
+                var ipHostEntries = Dns.GetHostEntry(hostName);
+                var ipEntries = ipHostEntries.AddressList
+                    .Where(ip => ip.IsSiteLocalAddress() && !ip.IsLoopbackAddress()).ToList();
+                if (ipEntries.Count == 0)
+                    return new Right<string, IPAddress>(ipEntries[0]);
+                return new Left<string, IPAddress>(
+                    $"Exactly one private address must be configured (found: [{string.Join(",", ipEntries)}])");
+            }
+        }
+
+        private readonly Configuration.Config _config;
+        private readonly string _cluster;
+        private readonly List<Tag> _tags = new List<Tag>();
+
+        private readonly AmazonECSClient _clientDoNotUseDirectly;
+
+        private AmazonECSClient EcsClient
+        {
+            get
+            {
+                if (_clientDoNotUseDirectly != null)
+                    return _clientDoNotUseDirectly;
+
+                return new AmazonECSClient(new AmazonECSConfig());
+            }
+        }
+        
+        public EcsServiceDiscovery(ActorSystem system)
+        {
+            _config = system.Settings.Config.GetConfig("akka.discovery.aws-api-ecs");
+            _cluster = _config.GetString("cluster");
+            var tags = _config.GetValue("tags").GetArray().Select(value => value.ToConfig()).ToList();
+            foreach (var tagValue in tags)
+            {
+                _tags.Add(new Tag
+                {
+                    Key = tagValue.GetString("key"),
+                    Value = tagValue.GetString("value")
+                });
+            }
+        }
+        
+        public override async Task<Resolved> Lookup(Lookup lookup, TimeSpan resolveTimeout)
+        {
+            using (var cts = new CancellationTokenSource(resolveTimeout))
+            {
+                var tasks = await ResolveTasks(EcsClient, _cluster, lookup.ServiceName, _tags, cts.Token);
+
+                var addresses = new List<ResolvedTarget>();
+                foreach (var task in tasks)
+                {
+                    foreach (var container in task.Containers)
+                    {
+                        foreach (var networkInterface in container.NetworkInterfaces)
+                        {
+                            var address = networkInterface.PrivateIpv4Address;
+                            var parsed = IPAddress.TryParse(address, out var ip) ;
+                            addresses.Add(new ResolvedTarget(address, null, parsed ? ip : null));
+                        }
+                    }
+                }
+                return new Resolved(lookup.ServiceName, addresses);
+            }
+        }
+
+        private static async Task<List<EcsTask>> ResolveTasks(
+            AmazonECSClient ecsClient,
+            string cluster,
+            string serviceName,
+            List<Tag> tags,
+            CancellationToken token)
+        {
+            var taskArns = await ListTaskArns(ecsClient, cluster, serviceName, token);
+            var tasks = await DescribeTasks(ecsClient, cluster, taskArns, token);
+            var tasksWithTags = tasks.Where(task =>
+                {
+                    foreach (var tag in tags)
+                    {
+                        if (task.Tags.Any(t => t.Key == tag.Key && t.Value == tag.Value))
+                            return false;
+                    }
+                    return true;
+                }
+            ).ToList();
+            return tasksWithTags;
+        }
+
+        private static async Task<List<string>> ListTaskArns(
+            AmazonECSClient ecsClient,
+            string cluster,
+            string serviceName,
+            CancellationToken token)
+        {
+            var listTaskRequest = new ListTasksRequest
+            {
+                Cluster = cluster,
+                ServiceName = serviceName,
+                DesiredStatus = DesiredStatus.RUNNING
+            };
+            
+            var accumulator = new List<string>();
+            do
+            {
+                var listTaskResult = await ecsClient.ListTasksAsync(listTaskRequest, token);
+                accumulator.AddRange(listTaskResult.TaskArns);
+                listTaskRequest.NextToken = listTaskResult.NextToken;
+            } while (listTaskRequest.NextToken != null);
+
+            return accumulator;
+        }
+
+        private static async Task<List<EcsTask>> DescribeTasks(
+            AmazonECSClient ecsClient,
+            string cluster,
+            List<string> taskArns,
+            CancellationToken token)
+        {
+            var include = new List<string> {TaskField.TAGS};
+            var accumulator = new List<EcsTask>();
+            // split batch into chunks of 100 requests
+            var lists = taskArns
+                .Select((value, index) => new {value, index})
+                .GroupBy(x => x.index / 100)
+                .Select(x => x.Select(v => v.value).ToList());
+            foreach (var arns in lists)
+            {
+                var response = await ecsClient.DescribeTasksAsync(new DescribeTasksRequest
+                {
+                    Cluster = cluster, 
+                    Tasks = arns,
+                    Include = include
+                }, token);
+                accumulator.AddRange(response.Tasks);
+            }
+
+            return accumulator;
+        }
+    }
+}
