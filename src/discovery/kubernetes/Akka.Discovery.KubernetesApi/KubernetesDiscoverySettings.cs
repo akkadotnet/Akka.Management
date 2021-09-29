6d46d90efe8977f077ac7da66429e735c6a9df41
Gregorius Soedharmo <arkatufus@yahoo.com>
2021-09-29 23:16:41 +0700

    Add kubernetes discovery (#145)
    * Update solution directory structure
    
    * Add Kubernetes discovery feature

---
+++ src/discovery/kubernetes/Akka.Discovery.KubernetesApi/KubernetesDiscoverySettings.cs
@@ -0,0 +1,42 @@
+﻿using Akka.Actor;
+
+#nullable enable
+namespace Akka.Discovery.KubernetesApi
+{
+    public sealed class KubernetesDiscoverySettings
+    {
+        private readonly string _podLabelSelector;
+        
+        public KubernetesDiscoverySettings(ExtendedActorSystem system)
+        {
+            var kubernetesApi = system.Settings.Config.GetConfig("akka.discovery.kubernetes-api");
+            ApiCaPath = kubernetesApi.GetString("api-ca-path");
+            ApiTokenPath = kubernetesApi.GetString("api-token-path");
+            ApiServiceHostEnvName = kubernetesApi.GetString("api-service-host-env-name");
+            ApiServicePortEnvName = kubernetesApi.GetString("api-service-port-env-name");
+            PodNamespacePath = kubernetesApi.GetString("pod-namespace-path");
+            PodNamespace = kubernetesApi.GetStringIfDefined("pod-namespace");
+            PodDomain = kubernetesApi.GetString("pod-domain");
+            _podLabelSelector = kubernetesApi.GetString("pod-label-selector");
+            RawIp = kubernetesApi.GetBoolean("use-raw-ip");
+            var containerName = kubernetesApi.GetString("container-name");
+            ContainerName = string.IsNullOrWhiteSpace(containerName) ? null : containerName;
+        }
+        
+        public string ApiCaPath { get; }
+        public string ApiTokenPath { get; }
+        public string ApiServiceHostEnvName { get; }
+        public string ApiServicePortEnvName { get; }
+        public string PodNamespacePath { get; }
+        public string PodNamespace { get; }
+        public string PodDomain { get; }
+        public string PodLabelSelector(string name)
+            => string.Format(_podLabelSelector, name);
+        public bool RawIp { get; }
+        public string? ContainerName { get; }
+
+        public override string ToString()
+            => $"Settings({ApiCaPath}, {ApiTokenPath}, {ApiServiceHostEnvName}, {ApiServicePortEnvName}, " +
+               $"{PodNamespacePath}, {PodNamespace}, {PodDomain})";
+    }
+}
