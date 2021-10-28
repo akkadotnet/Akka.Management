1e633b44ab0f3b90873958192f2902738a65a387
Gregorius Soedharmo <arkatufus@yahoo.com>
2021-10-28 09:12:07 +0700

    Fix settings need to be injected during Start

--- src/cluster.bootstrap/Akka.Management.Cluster.Bootstrap/ClusterBootstrap.cs
+++ src/cluster.bootstrap/Akka.Management.Cluster.Bootstrap/ClusterBootstrap.cs
@@ -59,6 +59,9 @@ namespace Akka.Management.Cluster.Bootstrap
             _selfContactPointTcs = new TaskCompletionSource<Uri>();
             
             _system = system;
+            _system.Settings.InjectTopLevelFallback(AkkaManagementProvider.DefaultConfiguration());
+            _system.Settings.InjectTopLevelFallback(DefaultConfiguration());
+            
             _log = Logging.GetLogger(system, typeof(ClusterBootstrap));
             _bootstrapStep= new AtomicReference<Internal.IBootstrapStep>(Internal.NotRunning.Instance);
             Settings = new ClusterBootstrapSettings(system.Settings.Config, _log);
