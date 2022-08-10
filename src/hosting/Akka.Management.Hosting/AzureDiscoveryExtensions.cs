5bfc8e33c0cbb11ebf45e77a8c97b98755a60d3d
Gregorius Soedharmo <arkatufus@yahoo.com>
2022-08-10 23:09:09 +0700

    Add Akka.Hosting support
    Added modules:
    - Akka.Management
    - Akka.Management.Cluster.Bootstrap
    - Akka.Discovery.Azure

---
+++ src/hosting/Akka.Management.Hosting/AzureDiscoveryExtensions.cs
@@ -0,0 +1,110 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="AzureDiscoveryExtensions.cs" company="Akka.NET Project">
+//      Copyright (C) 2013-2022 .NET Foundation <https://github.com/akkadotnet/akka.net>
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using System.Net;
+using Akka.Actor;
+using Akka.Configuration;
+using Akka.Discovery.Azure;
+using Akka.Hosting;
+
+namespace Akka.Management.Hosting
+{
+    public static class AzureDiscoveryExtensions
+    {
+        /// <summary>
+        ///     Adds Akka.Discovery.Azure support to the <see cref="ActorSystem"/>.
+        ///     Note that this only adds the discovery plugin, you will still need to add ClusterBootstrap for
+        ///     a complete solution.
+        /// </summary>
+        /// <param name="builder">
+        ///     The builder instance being configured.
+        /// </param>
+        /// <param name="connectionString">
+        ///     The connection string used to connect to Azure Table hosting the cluster membership table
+        /// </param>
+        /// <param name="serviceName">
+        ///     The service name assigned to the cluster.
+        /// </param>
+        /// <param name="publicHostname">
+        ///     The public IP/host of this node, usually for akka management. It will be used by other nodes to connect
+        ///     and query this node. Defaults to <see cref="Dns"/>
+        /// </param>
+        /// <param name="publicPort">
+        ///     The public port of this node, usually for akka management. It will be used by other nodes to connect
+        ///     and query this node. Defaults to 8558
+        /// </param>
+        /// <returns>
+        ///     The same <see cref="ActorSystem"/> instance originally passed in.
+        /// </returns>
+        public static AkkaConfigurationBuilder WithAzureDiscovery(
+            this AkkaConfigurationBuilder builder,
+            string connectionString,
+            string serviceName = null,
+            string publicHostname = null,
+            int? publicPort = null)
+        {
+            var setup = new AzureDiscoverySetup
+            {
+                ConnectionString = connectionString,
+                ServiceName = serviceName,
+                HostName = publicHostname,
+                Port = publicPort
+            };
+            return builder.WithAzureDiscovery(setup);
+        }
+
+        /// <summary>
+        ///     Adds Akka.Discovery.Azure support to the <see cref="ActorSystem"/>.
+        ///     Note that this only adds the discovery plugin, you will still need to add ClusterBootstrap for
+        ///     a complete solution.
+        /// </summary>
+        /// <param name="builder">
+        ///     The builder instance being configured.
+        /// </param>
+        /// <param name="configure">
+        ///     An action that modifies an <see cref="AzureDiscoverySetup"/> instance, used
+        ///     to configure Akka.Discovery.Azure.
+        /// </param>
+        /// <returns>
+        ///     The same <see cref="AkkaConfigurationBuilder"/> instance originally passed in.
+        /// </returns>
+        public static AkkaConfigurationBuilder WithAzureDiscovery(
+            this AkkaConfigurationBuilder builder,
+            Action<AzureDiscoverySetup> configure)
+        {
+            var setup = new AzureDiscoverySetup();
+            configure(setup);
+            return builder.WithAzureDiscovery(setup);
+        }
+
+        /// <summary>
+        ///     Adds Akka.Discovery.Azure support to the <see cref="ActorSystem"/>.
+        ///     Note that this only adds the discovery plugin, you will still need to add ClusterBootstrap for
+        ///     a complete solution.
+        /// </summary>
+        /// <param name="builder">
+        ///     The builder instance being configured.
+        /// </param>
+        /// <param name="setup">
+        ///     The <see cref="AzureDiscoverySetup"/> instance used to configure Akka.Discovery.Azure.
+        /// </param>
+        /// <returns>
+        ///     The same <see cref="AkkaConfigurationBuilder"/> instance originally passed in.
+        /// </returns>
+        public static AkkaConfigurationBuilder WithAzureDiscovery(
+            this AkkaConfigurationBuilder builder,
+            AzureDiscoverySetup setup)
+        {
+            builder.AddHocon(
+                ((Config)"akka.discovery.method = azure").WithFallback(AzureServiceDiscovery.DefaultConfig), 
+                HoconAddMode.Prepend);
+
+            builder.AddSetup(setup);
+            return builder;
+        }
+    }
+}
---
+++ src/hosting/Akka.Management.Hosting/ClusterBootstrapExtensions.cs
@@ -0,0 +1,143 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="ClusterBootstrapExtensions.cs" company="Akka.NET Project">
+//      Copyright (C) 2013-2022 .NET Foundation <https://github.com/akkadotnet/akka.net>
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using System.Linq;
+using Akka.Actor;
+using Akka.Configuration;
+using Akka.Hosting;
+using Akka.Management.Cluster.Bootstrap;
+
+namespace Akka.Management.Hosting
+{
+    public static class ClusterBootstrapExtensions
+    {
+        /// <summary>
+        ///     Adds Akka.Management.Cluster.Bootstrap support to the <see cref="ActorSystem"/>.
+        /// </summary>
+        /// <param name="builder">
+        ///     The builder instance being configured.
+        /// </param>
+        /// <param name="serviceName">
+        ///     Define this name to be looked up in service discovery for "neighboring" nodes
+        ///     If undefined, the name will be taken from the AKKA__CLUSTER__BOOTSTRAP__SERVICE_NAME
+        ///     environment variable or extracted from the ActorSystem name
+        /// </param>
+        /// <param name="serviceNamespace">
+        ///     Added as suffix to the service-name to build the effective-service name used in the contact-point
+        ///     service lookups If undefined, nothing will be appended to the service-name.
+        /// 
+        ///     Examples, set this to:
+        ///     "default.svc.cluster.local" or "my-namespace.svc.cluster.local" for kubernetes clusters.
+        /// </param>
+        /// <param name="portName">
+        ///     The portName passed to discovery. This should be set to the name of the port for Akka Management
+        /// </param>
+        /// <param name="newClusterEnabled">
+        ///     Cluster Bootstrap will always attempt to join an existing cluster if possible. However
+        ///     if no contact point advertises any seed-nodes a new cluster will be formed by the
+        ///     node with the lowest address as decided by the <see cref="LowestAddressJoinDecider"/>.
+        ///     Setting NewClusterEnabled to false after an initial cluster has formed is recommended to prevent new
+        ///     clusters forming during a network partition when nodes are redeployed or restarted.
+        /// </param>
+        /// <param name="autoStart">
+        ///     When set to true, cluster bootstrap will be started automatically on <see cref="ActorSystem"/> startup.
+        ///     Note that this will also automatically start Akka.Management on startup.
+        /// </param>
+        /// <returns>
+        ///     The same <see cref="AkkaConfigurationBuilder"/> instance originally passed in.
+        /// </returns>
+        public static AkkaConfigurationBuilder WithClusterBootstrap(
+            this AkkaConfigurationBuilder builder,
+            string serviceName = null,
+            string serviceNamespace = null,
+            string portName = null,
+            bool? newClusterEnabled = null,
+            bool autoStart = true)
+            => builder.WithClusterBootstrap(new ClusterBootstrapSetup
+            {
+                NewClusterEnabled = newClusterEnabled,
+                ContactPointDiscovery = new ContactPointDiscoverySetup
+                {
+                    ServiceName = serviceName,
+                    ServiceNamespace = serviceNamespace,
+                    PortName = portName
+                }
+            }, autoStart);
+        
+        /// <summary>
+        ///     Adds Akka.Management.Cluster.Bootstrap support to the <see cref="ActorSystem"/>.
+        /// </summary>
+        /// <param name="builder">
+        ///     The builder instance being configured.
+        /// </param>
+        /// <param name="configure">
+        ///     An action that modifies a <see cref="ClusterBootstrapSetup"/> instance, used
+        ///     to configure Akka.Management.Cluster.Bootstrap.
+        /// </param>
+        /// <param name="autoStart">
+        ///     When set to true, cluster bootstrap will be started automatically on <see cref="ActorSystem"/> startup.
+        ///     Note that this will also automatically start Akka.Management on startup.
+        /// </param>
+        /// <returns>
+        ///     The same <see cref="AkkaConfigurationBuilder"/> instance originally passed in.
+        /// </returns>
+        public static AkkaConfigurationBuilder WithClusterBootstrap(
+            this AkkaConfigurationBuilder builder,
+            Action<ClusterBootstrapSetup> configure,
+            bool autoStart = true)
+        {
+            var setup = new ClusterBootstrapSetup();
+            configure(setup);
+            return builder.WithClusterBootstrap(setup, autoStart);
+        }
+
+        /// <summary>
+        ///     Adds Akka.Management.Cluster.Bootstrap support to the <see cref="ActorSystem"/>.
+        /// </summary>
+        /// <param name="builder">
+        ///     The builder instance being configured.
+        /// </param>
+        /// <param name="setup">
+        ///     The <see cref="ClusterBootstrapSetup"/> that will be used to configure Akka.Management.Cluster.Bootstrap
+        /// </param>
+        /// <param name="autoStart">
+        ///     When set to true, cluster bootstrap will be started automatically on <see cref="ActorSystem"/> startup.
+        ///     Note that this will also automatically start Akka.Management on startup.
+        /// </param>
+        /// <returns>
+        ///     The same <see cref="AkkaConfigurationBuilder"/> instance originally passed in.
+        /// </returns>
+        public static AkkaConfigurationBuilder WithClusterBootstrap(
+            this AkkaConfigurationBuilder builder,
+            ClusterBootstrapSetup setup,
+            bool autoStart = true)
+        {
+            if (autoStart)
+            {
+                // Inject ClusterBootstrapProvider into akka.extensions
+                if (builder.Configuration.IsEmpty)
+                {
+                    var config = (Config)$"akka.extensions=[\"{typeof(ClusterBootstrapProvider).AssemblyQualifiedName}\"]";
+                    builder.AddHocon(config, HoconAddMode.Prepend);
+                }
+                else
+                {
+                    var extensions = builder.Configuration.Value.GetStringList("akka.extensions").ToList();
+                    if (extensions.All(s => !s.Contains(nameof(ClusterBootstrapProvider))))
+                    {
+                        extensions.Add(typeof(ClusterBootstrapProvider).AssemblyQualifiedName);
+                        var config = (Config)$"akka.extensions=[{string.Join(",", extensions.Select(s => $"\"{s}\""))}]";
+                        builder.AddHocon(config, HoconAddMode.Prepend);
+                    }
+                }
+            }
+        
+            builder.AddSetup(setup);
+            return builder;
+        }
+    }
+}
