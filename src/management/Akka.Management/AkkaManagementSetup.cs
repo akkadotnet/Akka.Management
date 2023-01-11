e71112e1396524afe09f41bf37489424fa05603c
Gregorius Soedharmo <arkatufus@yahoo.com>
2023-01-12 03:43:11 +0700

    [Management] Change extension argument from Setup to Options

--- src/management/Akka.Management/AkkaManagementSetup.cs
+++ src/management/Akka.Management/AkkaManagementSetup.cs
@@ -78,12 +78,12 @@ namespace Akka.Management
         ///
         /// RouteProviders["health-check"] = null; 
         /// </summary>
-        public Dictionary<string, Type> RouteProviders { get; } = new ();
+        public Dictionary<string, Type?> RouteProviders { get; } = new ();
 
         public HttpSetup WithRouteProvider<T>(string name) where T : IManagementRouteProvider
         {
             var type = typeof(T);
-            if (RouteProviders.Values.Contains(type))
+            if (RouteProviders.ContainsValue(type))
                 throw new ConfigurationException($"The route provider of type {type.Name} already added");
             
             RouteProviders[name] = type;
