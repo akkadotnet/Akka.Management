6d5402dab26c66b79ee3f087e881add02ca8b50e
Gregorius Soedharmo <arkatufus@yahoo.com>
2024-06-27 21:34:26 +0700

    Update Discovery.KubernetesApi to support multi-config (#2596)

--- src/discovery/kubernetes/Akka.Discovery.KubernetesApi/Extensions.cs
+++ src/discovery/kubernetes/Akka.Discovery.KubernetesApi/Extensions.cs
@@ -5,6 +5,10 @@
 // </copyright>
 //-----------------------------------------------------------------------
 
+using System.Linq;
+using Akka.Configuration.Hocon;
+using Akka.Util.Internal;
+
 namespace Akka.Discovery.KubernetesApi
 {
     internal static class ConfigExtensions
@@ -13,6 +17,31 @@ namespace Akka.Discovery.KubernetesApi
         {
             var value = config.GetString(key);
             return string.IsNullOrWhiteSpace(value) || value.Equals($"<{key}>") ? null : value;
+        }
+        
+        internal static Configuration.Config MoveTo(this Configuration.Config config, string path)
+        {
+            var rootObj = new HoconObject();
+            var rootValue = new HoconValue();
+            rootValue.Values.Add(rootObj);
+
+            var lastObject = rootObj;
+
+            var keys = path.SplitDottedPathHonouringQuotes().ToArray();
+            for (var i = 0; i < keys.Length - 1; i++)
+            {
+                var key = keys[i];
+                var innerObject = new HoconObject();
+                var innerValue = new HoconValue();
+                innerValue.Values.Add(innerObject);
+
+                lastObject.GetOrCreateKey(key);
+                lastObject.Items[key] = innerValue;
+                lastObject = innerObject;
+            }
+            lastObject.Items[keys[keys.Length - 1]] = config.Root;
+
+            return new Configuration.Config(new HoconRoot(rootValue));
         }
     }
 
