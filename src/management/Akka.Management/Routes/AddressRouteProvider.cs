eb307c9af2cbe9afdffdafd2be0a5c697598b67e
Gregorius Soedharmo <arkatufus@yahoo.com>
2024-07-02 21:22:05 +0700

    Add ClusterClientReceptionist actor path and Remote address endpoint

---
+++ src/management/Akka.Management/Routes/AddressRouteProvider.cs
@@ -0,0 +1,56 @@
+﻿using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.Http.Dsl;
+using Akka.Management.Dsl;
+using Akka.Remote;
+using Ceen;
+using Newtonsoft.Json;
+using Route = System.ValueTuple<string, Akka.Http.Dsl.HttpModuleBase>;
+
+namespace Akka.Management.Routes;
+
+public class AddressRouteProvider: IManagementRouteProvider
+{
+    public Route[] Routes(ManagementRouteProviderSettings settings)
+    {
+        return new HttpAddressRoutes().Routes;
+    }
+}
+
+internal class HttpAddressRoutes : HttpModuleBase
+{
+    public Route[] Routes => new Route[] { ("/remote/address", this) };
+    
+    public override async Task<bool> HandleAsync(IAkkaHttpContext context)
+    {
+        if (context.HttpContext.Request.Method.ToLowerInvariant() != "get")
+            return false;
+        
+        // Check that remoting is in effect
+        if (((ExtendedActorSystem)context.ActorSystem).Provider is not IRemoteActorRefProvider actorProvider)
+        {
+            context.HttpContext.Response.StatusCode = HttpStatusCode.ServiceUnavailable;
+            var response = JsonConvert.SerializeObject(new
+            {
+                error = new
+                {
+                    reason = "not available",
+                    message = "Remoting is not available"
+                },
+                code = (int)HttpStatusCode.ServiceUnavailable,
+                message = "Remoting is not available"
+            });
+            await context.HttpContext.Response.WriteAllJsonAsync(response);
+            return true;
+        }
+        
+        var json = JsonConvert.SerializeObject(new
+        {
+            Address = actorProvider.DefaultAddress.ToString()
+        });
+        
+        await context.HttpContext.Response.WriteAllJsonAsync(json);
+        
+        return true;
+    }
+}
---
+++ src/management/Akka.Management/Routes/ClusterClientReceptionistRouteProvider.cs
@@ -0,0 +1,91 @@
+﻿using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.Cluster;
+using Akka.Http.Dsl;
+using Akka.Management.Dsl;
+using Akka.Remote;
+using Ceen;
+using Newtonsoft.Json;
+using Route = System.ValueTuple<string, Akka.Http.Dsl.HttpModuleBase>;
+
+namespace Akka.Management.Routes;
+
+public class ClusterClientReceptionistRouteProvider: IManagementRouteProvider
+{
+    public Route[] Routes(ManagementRouteProviderSettings settings)
+    {
+        return new HttpClusterClientReceptionistRoutes().Routes;
+    }
+}
+
+internal class HttpClusterClientReceptionistRoutes : HttpModuleBase
+{
+    public Route[] Routes => new Route[] { ("/cluster-client/receptionist", this) };
+    
+    public override async Task<bool> HandleAsync(IAkkaHttpContext context)
+    {
+        if (context.HttpContext.Request.Method.ToLowerInvariant() != "get")
+            return false;
+        
+        // Check that remoting is in effect
+        if (((ExtendedActorSystem)context.ActorSystem).Provider is not IRemoteActorRefProvider actorProvider)
+        {
+            context.HttpContext.Response.StatusCode = HttpStatusCode.ServiceUnavailable;
+            var response = JsonConvert.SerializeObject(new
+            {
+                error = new
+                {
+                    reason = "not available",
+                    message = "Remoting is not available"
+                },
+                code = (int)HttpStatusCode.ServiceUnavailable,
+                message = "Remoting is not available"
+            });
+            await context.HttpContext.Response.WriteAllJsonAsync(response);
+            return true;
+        }
+        
+        // Check that clustering is in effect
+        if (((ExtendedActorSystem)context.ActorSystem).Provider is not IClusterActorRefProvider)
+        {
+            context.HttpContext.Response.StatusCode = HttpStatusCode.ServiceUnavailable;
+            var response = JsonConvert.SerializeObject(new
+            {
+                error = new
+                {
+                    reason = "not available",
+                    message = "Clustering is not available"
+                },
+                code = (int)HttpStatusCode.ServiceUnavailable,
+                message = "Clustering is not available"
+            });
+            await context.HttpContext.Response.WriteAllJsonAsync(response);
+            return true;
+        }
+        
+        // Check that ClusterClientReceptionist is available
+        var config = context.ActorSystem.Settings.Config.GetConfig("akka.cluster.client.receptionist");
+        if(config is null)
+        {
+            context.HttpContext.Response.StatusCode = HttpStatusCode.ServiceUnavailable;
+            var response = JsonConvert.SerializeObject(new
+            {
+                error = new
+                {
+                    reason = "not available",
+                    message = "ClusterClientReceptionist is not available"
+                },
+                code = (int)HttpStatusCode.ServiceUnavailable,
+                message = "ClusterClientReceptionist is not available"
+            });
+            await context.HttpContext.Response.WriteAllJsonAsync(response);
+            return true;
+        }
+        
+        var actorPath = new RootActorPath(actorProvider.DefaultAddress) / "system" / config.GetString("name"); 
+        var json = JsonConvert.SerializeObject(new { ReceptionistPath = actorPath.ToString() });
+        await context.HttpContext.Response.WriteAllJsonAsync(json);
+        
+        return true;
+    }
+}
