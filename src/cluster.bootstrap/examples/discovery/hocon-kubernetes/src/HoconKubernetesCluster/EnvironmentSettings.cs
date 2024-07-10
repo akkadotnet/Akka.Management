73b268469d023b03a098bdc661e5a6a5ab48377b
Gregorius Soedharmo <arkatufus@yahoo.com>
2024-07-11 03:18:52 +0700

    Sample: KubernetesApi cluster bootstraping via HOCON (#2641)

---
+++ src/cluster.bootstrap/examples/discovery/hocon-kubernetes/src/HoconKubernetesCluster/EnvironmentSettings.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using Akka.Configuration;
+
+namespace HoconKubernetesCluster;
+
+public record EnvironmentSettings(
+    string? RemotingHostName,
+    string? RemotingPort,
+    string? ManagementHostName,
+    string? ManagementPort,
+    string? BootstrapServiceName,
+    string? BootstrapPortName)
+{
+    private const string RemoteHostNameKey = "AKKA_REMOTE_HOSTNAME";
+    private const string RemotePortKey = "AKKA_REMOTE_PORT";
+    private const string ManagementHostNameKey = "AKKA_MANAGEMENT_HOSTNAME";
+    private const string ManagementPortKey = "AKKA_MANAGEMENT_PORT";
+    private const string BootstrapServiceNameKey = "AKKA_BOOTSTRAP_SERVICE_NAME";
+    private const string BootstrapPortNameKey = "AKKA_BOOTSTRAP_PORT_NAME";
+
+    public Config ToConfig()
+    {
+        var sb = new StringBuilder();
+        if (RemotingHostName is not null)
+            sb.AppendLine($"akka.remote.dot-netty.tcp.public-hostname = {RemotingHostName}");
+        
+        if(RemotingPort is not null)
+            sb.AppendLine($"akka.remote.dot-netty.tcp.port = {RemotingPort}");
+
+        if (ManagementHostName is not null)
+            sb.AppendLine($"akka.management.hostname = {ManagementHostName}");
+
+        if (ManagementPort is not null)
+            sb.AppendLine($"akka.management.port = {ManagementPort}");
+
+        if (BootstrapServiceName is not null)
+            sb.AppendLine($"akka.management.cluster.bootstrap.contact-point-discovery.service-name = {BootstrapServiceName}");
+
+        if (BootstrapPortName is not null)
+            sb.AppendLine($"akka.management.cluster.bootstrap.contact-point-discovery.port-name = {BootstrapPortName}");
+
+        return sb.Length == 0 ? Config.Empty : sb.ToString();
+    }
+
+    public static EnvironmentSettings Create()
+        => new (
+            RemotingHostName: Environment.GetEnvironmentVariable(RemoteHostNameKey),
+            RemotingPort: Environment.GetEnvironmentVariable(RemotePortKey),
+            ManagementHostName: Environment.GetEnvironmentVariable(ManagementHostNameKey),
+            ManagementPort: Environment.GetEnvironmentVariable(ManagementPortKey),
+            BootstrapServiceName: Environment.GetEnvironmentVariable(BootstrapServiceNameKey),
+            BootstrapPortName: Environment.GetEnvironmentVariable(BootstrapPortNameKey)
+        );
+}
