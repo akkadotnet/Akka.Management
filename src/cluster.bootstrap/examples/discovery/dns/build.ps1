fbec5dce45c93df79ff8112e0c6bb672aaf9511e
Pavel Anpin <pavel@anpin.fyi>
2025-07-14 15:25:13 -0300

    use docker-compose to run examples as it is more common

--- src/cluster.bootstrap/examples/discovery/dns/build.ps1
+++ src/cluster.bootstrap/examples/discovery/dns/build.ps1
@@ -10,12 +10,12 @@ if ($recordType -ne "srv" -and $recordType -ne "a" -and $recordType -ne "aaaa") 
     exit 1
 }
 Write-Output "Cleaning up previous containers (ignore errors if containers do not exist)...."
-podman-compose -f "$(pwd)/src/docker-compose.srv.yml" down
-podman-compose -f "$(pwd)/src/docker-compose.aaaa.yml" down
-podman-compose -f "$(pwd)/src/docker-compose.a.yml" down
+docker-compose -f "$(pwd)/src/docker-compose.srv.yml" down
+docker-compose -f "$(pwd)/src/docker-compose.aaaa.yml" down
+docker-compose -f "$(pwd)/src/docker-compose.a.yml" down
 
 Write-Output "Building and publishing example..."
 dotnet publish --os linux --arch x64 -c Release /t:PublishContainer ./src/DnsCluster.csproj
 
 Write-Output "Start $recordType example"
-podman-compose -f "$(pwd)/src/docker-compose.$recordType.yml" up --build 
+docker-compose -f "$(pwd)/src/docker-compose.$recordType.yml" up --build 
